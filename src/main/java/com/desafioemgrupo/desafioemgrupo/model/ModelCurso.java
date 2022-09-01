7cc43a8871a57d4bb35dafa68fae162bb552ee6d
pauloren7 <paulorenan1515@gmail.com>
2022-09-01 18:21:40 -0300

    atualizações

--- src/main/java/com/desafioemgrupo/desafioemgrupo/model/ModelCurso.java
+++ src/main/java/com/desafioemgrupo/desafioemgrupo/model/ModelCurso.java
@@ -6,16 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "curso")
 public class ModelCurso {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/desafioemgrupo/desafioemgrupo/repository/RespostaRepository.java
+++ src/main/java/com/desafioemgrupo/desafioemgrupo/repository/RespostaRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RespostaRepository extends JpaRepository<RespostaModel, Long> {
+public interface RespostaRepository extends JpaRepository<RespostaModel, Long> {
 
 
 }
